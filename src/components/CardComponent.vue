0344055c0a4057cf847daa431d904589fe44a070
Ddanilo9 <interiordanilo3@gmail.com>
2022-09-07 17:18:48 +0200

    creazione card component

---
+++ src/components/CardComponent.vue
@@ -0,0 +1,45 @@
+<template>
+      <div class="info-card">
+                <img :src="info.poster" alt="">
+                <h3>{{info.title}}</h3>
+                <span>{{info.author}}</span>
+                <span>{{info.year}}</span>
+            </div>
+  </template>
+  
+  <script>
+  
+  
+  export default {
+    name: 'CharacterComponent',
+  
+    props: {
+      info: Object
+    },
+   
+  }
+  </script>
+  
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.info-card{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    h3{
+        padding: 10px 0;
+        text-align: center;
+    }
+    span{
+        padding: 2px 0;
+    }
+}
+    img{
+    max-width: 100%;
+    cursor: pointer;
+    aspect-ratio: 1/1;
+    }
+ 
+  </style>
+  
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -2,12 +2,7 @@
     <div>
      <div class="container grid">
         <div v-for="(disco, i) in listaDischi" :key="i" class="card">
-            <div class="info-card">
-                <img :src="disco.poster" alt="">
-                <h3>{{disco.title}}</h3>
-                <span>{{disco.author}}</span>
-                <span>{{disco.year}}</span>
-            </div>
+          <CardComponent :info="disco"></CardComponent>
         </div>
      </div>
     </div>
@@ -15,6 +10,8 @@
   
   <script>
   import axios from "axios"
+
+  import CardComponent from './CardComponent.vue'
   
   export default {
     name: 'MainContent',
@@ -29,6 +26,9 @@
         console.log(res.data)
         this.listaDischi = res.data.response
       });
+    },
+    components: {
+        CardComponent
     }
   }
   </script>
@@ -45,23 +45,6 @@
             background-color: rgba(46,58,70,255);
             color: white;
             padding: 15px;
-                .info-card{
-                    display: flex;
-                    flex-direction: column;
-                    align-items: center;
-
-                    h3{
-                        padding: 10px 0;
-                        text-align: center;
-                    }
-                    span{
-                        padding: 2px 0;
-                    }
-                }
-        }
-        img{
-            max-width: 100%;
-            cursor: pointer;
         }
   }
   </style>
